1206fb3b50e35369b1fc6846c860339262f34287
kamaldalakoti <kamaldalakoti900@gmail.com>
2021-11-26 20:59:12 +0530

    1

--- Tour_and_Travel/urls.py
+++ Tour_and_Travel/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('',include( 'Home.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.MEDIA_URL,
