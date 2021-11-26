a05276aa56c11bd3f17ce9b4d40bd9226e1ae2e4
kamaldalakoti <kamaldalakoti900@gmail.com>
2021-11-26 21:22:03 +0530

    Update settings.py

--- Tour_and_Travel/settings.py
+++ Tour_and_Travel/settings.py
@@ -135,4 +135,5 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
